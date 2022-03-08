df0b296c6cd40d122d61ada3373444608125487b
ToNgPhUc <100405665+ToNgPhUc@users.noreply.github.com>
2022-03-08 10:35:47 +0700

    nop bai

---
+++ module1/ss7_bien_va_toan_tu/thuc_hanh/Bai2SuDungToanTu.js
@@ -0,0 +1,9 @@
+let inputwidth;
+let inputHeight;
+inputwidth= prompt('enter the width');
+inputHeight= prompt('enter the height');
+let width= parseInt(inputwidth);
+let height =parseInt(inputHeight)
+let aria =width*height;
+document.write('aria la' +aria)
+
