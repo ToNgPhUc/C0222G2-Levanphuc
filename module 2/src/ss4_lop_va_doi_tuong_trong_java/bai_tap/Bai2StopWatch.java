863d67b17863a0ed01cecbeb239ccac41d256341
ToNgPhUc <100405665+ToNgPhUc@users.noreply.github.com>
2022-04-06 22:46:29 +0700

    nop bai

--- module 2/src/ss4_lop_va_doi_tuong_trong_java/bai_tap/Bai2StopWatch.java
+++ module 2/src/ss4_lop_va_doi_tuong_trong_java/bai_tap/Bai2StopWatch.java
@@ -1,17 +1,18 @@
 package ss4_lop_va_doi_tuong_trong_java.bai_tap;
 
 
-
-
 public class Bai2StopWatch {
 
     private long startTime;
     private long endTime;
-    int starTime1;
 
     public Bai2StopWatch() {
-        Bai2StopWatch stop = new Bai2StopWatch();
-        System.out.println(stop.getElapsedTime());
+
+    }
+
+    public Bai2StopWatch(long startTime, long endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
     }
 
     public long getStartTime() {
@@ -29,23 +30,31 @@ public class Bai2StopWatch {
     public void setEndTime(long endTime) {
         this.endTime = endTime;
     }
-    public void StarTime(){
-        System.out.println(java.time.LocalTime.now());
+
+    public void Start() {
+        this.startTime = System.currentTimeMillis();
     }
 
-
-    public void Star(long startTime,long endTime) {
-this.startTime= startTime;
-    }
-
-    public void Stop(long startTime,long endTime) {
-        this.endTime = endTime;
+    public void Stop() {
+        this.endTime = System.currentTimeMillis();
 
     }
 
     public long getElapsedTime() {
-        return this.endTime - this.startTime;
+return this.endTime-this.startTime;
     }
 
+    public static void main(String[] args) {
+       Bai2StopWatch stop=new Bai2StopWatch();
+       stop.Start();
+       int a=100;
+       int b;
 
+        for (int i = 0; i < 10000000; i++) {
+      a=100+100;
+
+        }stop.Stop();
+        System.out.println(stop.getElapsedTime());
+
+    }
 }
