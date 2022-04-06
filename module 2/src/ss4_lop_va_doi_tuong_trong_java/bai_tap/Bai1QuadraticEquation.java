6f14144e070bdc3a6df4ae3a350a73a263825ccf
ToNgPhUc <100405665+ToNgPhUc@users.noreply.github.com>
2022-04-06 11:51:27 +0700

    nop bai

--- module 2/src/ss4_lop_va_doi_tuong_trong_java/bai_tap/Bai1QuadraticEquation.java
+++ module 2/src/ss4_lop_va_doi_tuong_trong_java/bai_tap/Bai1QuadraticEquation.java
@@ -2,6 +2,8 @@ package ss4_lop_va_doi_tuong_trong_java.bai_tap;
 
 import ss4_lop_va_doi_tuong_trong_java.thuc_hanh.Bai1RectangularClass;
 
+import java.util.Scanner;
+
 //phuong trinh bac hai
     public class Bai1QuadraticEquation {
         double a;
@@ -40,6 +42,28 @@ import ss4_lop_va_doi_tuong_trong_java.thuc_hanh.Bai1RectangularClass;
             return " value a: "+a+"\n"+
                     " value b: "+b+"\n"+
                     " value c: "+c+"\n";
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a");
+        double a= Double.parseDouble(sc.nextLine());
+        System.out.println("enter b");
+        double b= Double.parseDouble(sc.nextLine());
+        System.out.println("enter c");
+        double c= Double.parseDouble(sc.nextLine());
+        Bai1QuadraticEquation quadraticEquation= new Bai1QuadraticEquation(a,b,c);
+        System.out.println(quadraticEquation.Display());
+
+        if (quadraticEquation.getDiscriminant()>0){//nếu delta > 0
+            System.out.println(quadraticEquation.getRoot1());//xuất ra 2 nghiệm root1 và root 2
+            System.out.println(quadraticEquation.getRoot2());
+        }else if (quadraticEquation.getDiscriminant()==0){//nếu delta = 0
+            System.out.println(quadraticEquation.getRoot1());//xuất ra 1 nghiệm
+
+        }else {
+            System.out.println("The equation has no roots");//delta <0 phương trình vô ngiệm
+
+        }
     }
 
 }
