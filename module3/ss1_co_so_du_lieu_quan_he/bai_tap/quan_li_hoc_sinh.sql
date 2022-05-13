6d696be0634ad83fd5c44342269e53296be23f7c
ToNgPhUc <100405665+ToNgPhUc@users.noreply.github.com>
2022-05-13 11:09:45 +0700

    nop bai

---
+++ module3/ss1_co_so_du_lieu_quan_he/bai_tap/quan_li_hoc_sinh.sql
@@ -0,0 +1,10 @@
+create table student_management.class(
+id int,
+name_class varchar(100)
+);
+create table student_management.teacher(
+id int, 
+name_teacher varchar(100),
+age int,
+country varchar(100)
+);
